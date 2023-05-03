4ff19f330107d803eade0395b7e775a921a0c958
Severin Buhler <sevi_buehler@hotmail.com>
2023-05-03 15:22:32 +0800

    added onchain balance watcher

--- src/1_lnd/lndNode/OnchainBalanceMonitor.ts
+++ src/1_lnd/lndNode/OnchainBalanceMonitor.ts
@@ -1,11 +1,24 @@
+import { GrenacheClient } from "blocktank-worker2";
 import { LndNode } from "./LndNode";
+import { Config } from "../../1_config/Config";
+
+
+const config = Config.get()
+
 
 export class OnchainBalanceMonitor {
+    private isNodeBelow: Map<string, boolean> = new Map()
     public nodes: LndNode[]
     private watchPeriodMs = 30*1000; // 30s
     private alertThresholdSat: number
     private interval: NodeJS.Timer
+    private grenacheClient = new GrenacheClient(config.grapeUrl)
+    private slackService: any;
+
+    private isFirstRun = true
     async watch(nodes: LndNode[], alertThresholdSat: number) {
+        this.grenacheClient.start()
+        this.slackService = this.grenacheClient.encapsulateWorker('svc:slack2')
         this.nodes = nodes
         this.alertThresholdSat = alertThresholdSat
 
@@ -19,23 +32,43 @@ export class OnchainBalanceMonitor {
         if (this.interval) {
             clearInterval(this.interval)
         }
+        this.grenacheClient.stop()
     }
 
     async watchOnce() {
         for (const node of this.nodes) {
             try {
                 const balanceSat = await node.getOnchainBalance()
-                if (balanceSat < this.alertThresholdSat) {
-                    await this.onBalanceBelowTreshhold(node, balanceSat)
+                const isBelow = balanceSat < this.alertThresholdSat
+                const stateChanged = this.isNodeBelow.get(node.publicKey) !== isBelow
+                this.isNodeBelow.set(node.publicKey, isBelow)
+                if (stateChanged) {
+                    await this.onBalanceStateChanged(node, balanceSat)
                 }
             } catch(e) {
                 console.error('Error getting onchain balance', e)
             }
         }
+        this.isFirstRun = false
     }
 
-    private async onBalanceBelowTreshhold(node: LndNode, balanceSat: number) {
-        console.log(`Onchain balance of node ${node.alias} ${node.publicKey} is below threshold of ${this.alertThresholdSat.toLocaleString()}sat:  ${balanceSat.toLocaleString()}sat.`)
+    private formatSats(satoshi: number): string {
+        const btc = satoshi / (100*1000*1000)
+        return 'BTC' + btc.toFixed(8)
+    }
 
+    private async onBalanceStateChanged(node: LndNode, balanceSat: number) {
+        const formatedBtc = this.formatSats(balanceSat)
+        if (this.alertThresholdSat > balanceSat) {
+            const message = `Onchain balance of node ${node.alias} ${node.publicKey} are below threshold of ${this.formatSats(this.alertThresholdSat)}:  ${formatedBtc}.`
+            console.log(message)
+            await this.slackService.sendMessage('blocktankInstant', 'warning', 'Onchain funds are low.', message, undefined)
+            return
+        }
+        if (!this.isFirstRun){ // Dont send recovered on watcher startup.
+            const message = `Onchain balance of node ${node.alias} ${node.publicKey} recovered to ${formatedBtc}.`
+            console.log(message)
+            await this.slackService.sendMessage('blocktankInstant', 'success', 'Onchain recovered', message, undefined)
+        }
     }
 }
